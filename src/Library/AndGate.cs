1313996d29a5dab737814129f0d141c8c7b7cc74
DiegoAbarno <DIEGO.ABARNO@correo.ucu.edu.uy>
2025-09-22 11:29:53 -0300

    Create clas AndGate

---
+++ src/Library/AndGate.cs
@@ -0,0 +1,51 @@
+﻿namespace Library;
+
+public class AndGate
+{
+    private string nombre;
+    private List<int> entrada;
+    private int salida;
+
+    public string Nombre
+    {
+        get { return this.nombre; }
+        private set { this.nombre = value; }
+    }
+    
+    public List<int> Entrada
+    {
+        get { return this.entrada; }
+        set { this.entrada = value; }
+    }
+
+    public int Salida
+    {
+        get { return this.salida; }
+        private set { this.salida = value; }
+    }
+
+    public AndGate(string nombre)
+    {
+        this.Nombre = nombre;
+        this.Entrada = new List<int>();
+        this.Salida = 0;
+    }
+
+    public void Evaluar()
+    {
+        if (this.entrada.Contains(0))
+        {
+            this.salida = 0;
+        }
+        else
+        {
+            this.salida = 1;
+        }
+    }
+
+    public void AgregarEntradas(int entrada)
+    {
+        this.entrada.Add(entrada);
+    }
+    
+}
